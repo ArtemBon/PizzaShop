5c078ec0adae3e0a41d38a5d291d0acc19a27f28
Artem Bondarets <tyom.bond@gmail.com>
2019-08-03 15:44:03 +0300

    Add counter of products in cart

--- public/app.js
+++ public/app.js
@@ -16,4 +16,12 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	var total = 0;
+	for (var i = 1; i <= 3; i++)
+	{
+		var key = 'product_' + i;
+		total += window.localStorage.getItem(key) * 1;
+	}
+	alert('There are ' + total + ' pizzas in cart.');
 }
