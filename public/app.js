67339695f984ccf43b44392baaa8a63a6334378e
Artem Bondarets <tyom.bond@gmail.com>
2019-08-03 21:20:06 +0300

    Add button, /cart handler stub, cart_get_orders function

--- public/app.js
+++ public/app.js
@@ -17,13 +17,35 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
+	alert('There are ' + cart_get_number_of_items() + ' pizzas in cart.');
+}
+
+function cart_get_number_of_items()
+{
 	var total = 0;
+
 	for (var i=0, len = window.localStorage.length; i<len; i++)
 	{
-		key = window.localStorage.key(i);
+		var key = window.localStorage.key(i);
 		if ((/^product_/).test(key)){
 			total += window.localStorage.getItem(key) * 1;
 		}
 	}
-	alert('There are ' + total + ' pizzas in cart.');
+
+	return total;
+}
+
+function cart_get_orders()
+{
+	var total = '';
+
+	for (var i=0, len = window.localStorage.length; i<len; i++)
+	{
+		var key = window.localStorage.key(i);
+		if ((/^product_/).test(key)){
+			total += key + '=' + window.localStorage.getItem(key) + ',';
+		}
+	}
+
+	return total;
 }
