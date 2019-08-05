3026c71233dc09480302c650d37151b335faf900
Artem Bondarets <tyom.bond@gmail.com>
2019-08-05 21:54:24 +0300

    Minor changes

--- public/app.js
+++ public/app.js
@@ -1,14 +1,3 @@
-function something()
-{
-	var x = window.localStorage.getItem('asdf');
-
-	x = x * 1 + 1;
-
-	window.localStorage.setItem('asdf', x);
-
-	alert(x);
-}
-
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
