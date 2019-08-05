b3423a9b75cdedd523eadd8a1c47999c8968d795
Artem Bondarets <tyom.bond@gmail.com>
2019-08-05 12:42:22 +0300

    Add cancel button and mock javascript handler

--- public/app.js
+++ public/app.js
@@ -64,3 +64,9 @@ function update_orders_button()
 	var text = 'Cart (' + cart_get_number_of_items() + ')';
 	$('#orders_button').val(text);
 }
+
+function cancel_order()
+{
+	window.localStorage.clear();
+	return false;
+}
